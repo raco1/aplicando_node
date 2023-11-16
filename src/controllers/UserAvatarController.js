e52c70fa4ed65467769c57e3b374d9579092c681
rafael <rafaelcoelho2711@gmail.com>
2023-11-16 15:29:06 -0300

    user avatar created

---
+++ src/controllers/UserAvatarController.js
@@ -0,0 +1,30 @@
+const knex = require("knex")
+const AppError = require("../utils/AppError")
+const DiskStorage = require("../provider/DiskStorage")
+
+class UserAvatarController {
+    async update(request, response){
+        const user_id = request.user.user_id
+        const avatarFileName = request.file.filename
+        const diskStorage = new DiskStorage();
+
+        const user =  await knex("users").where({ id: user_id }).first()
+
+        if(!user){
+            throw new AppError("Somente usuários autênticados podem alterar a foto de perfil.")
+        }
+
+        if(user.avatar){
+            await diskStorage.deleteFile(user.avatar)
+        }
+
+        const filename = await diskStorage.save(avatarFileName)
+        user.avatar = filename
+
+        await knex("users").update(user).where({ id: user_id })
+
+        return response.json(user)
+    }
+}
+
+module.exports = UserAvatarController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,19 @@
 const { Router } = require("express");
-
 const ensureAuthenticated = require("../middleware/EnsureAuthentication")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const usersRoutes = Router();
 
 const UserController = require("../controllers/UserController");
+const UserAvatarController = require("../controllers/UserAvatarController")
+const upload = multer(uploadConfig.MULTER)
 
 const userController = new UserController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", userController.create);
 usersRoutes.put("/", ensureAuthenticated, userController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes;
