6afdddb3c8f3b8ad8c6706428d2a63d467f94396
rafael <rafaelcoelho2711@gmail.com>
2023-11-16 10:28:32 -0300

    organização de importaçao

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,12 @@
-const { Router } = require("express");
-
-const usersRoutes = require("./user.routes");
-
 const sessionsRoutes = require("./sessions.routes");
-
 const notesRoutes = require("./notes.routes");
-
+const usersRoutes = require("./user.routes");
 const tagsRoutes = require("./tags.routes");
-
+const { Router } = require("express");
 const routes = Router();
 
-routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes)
+routes.use("/users", usersRoutes);
 routes.use("/notes", notesRoutes);
 routes.use("/tags", tagsRoutes);
 
