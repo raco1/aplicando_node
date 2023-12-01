ede5f1ea87d59d7cc235a41a748f1e3874441208
rafael <rafaelcoelho2711@gmail.com>
2023-12-01 10:02:26 -0300

    repository in memory criado

---
+++ src/repositories/UserRepositoryInMemory.js
@@ -0,0 +1,19 @@
+class UserRepositoryInMemory {
+    users = []
+    async create({ name, email, password }){
+        const user = {
+            id: Math.floor(Math.random() * 1000) + 1,
+            name,
+            email,
+            password
+        }
+        this.users.push(user)
+
+        return user
+    }
+
+    async findByEmail(email){
+        return this.users.find(user => this.users.email === email)
+    }
+}
+module.exports= UserRepositoryInMemory
